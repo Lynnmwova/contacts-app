7dd89a2b8dc2ccf317f374115319efbd7f257d5e
Lynnmwova <82355571+Lynnmwova@users.noreply.github.com>
2022-11-23 14:10:42 +0300

    state management commits

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,35 +1,33 @@
 import React,{Component} from 'react'
 
-
-class ListContacts extends Component{
-
-  render(){
-    
-    return (
-      <ol className= 'contact-list'> 
-      { this.props.contacts.map( (contact) => ( 
+function ListContacts (props){ 
+  return(
+    <ol className= 'contact-list'> 
+    {props.contacts.map( (contact) => ( 
 <li key={contact.id } className='contact-list-item'> 
 {contact.name}
 <div className='contact-avatar' 
-  style = {{
-    backgroundImage: `url(${contact.avatarURL})`
-  }}
+style = {{
+  backgroundImage: `url(${contact.avatarURL})`
+}}
 />
 <div className='contact-details'>
-  <p>{contact.name}</p>
-  <p>{contact.email}</p>
+<p>{contact.name}</p>
+<p>{contact.email}</p>
 </div>
 
-<button className='contact-remove'> Remove </button>
+<button 
+onClick={()=>props.onDeleteContact(contact)} 
+className='contact-remove'> Remove </button>
 
 </li>
-      ) 
-      )}
-      </ol>
-    )
-  }
-
+    ) 
+    )}
+    </ol>
+  )
 }
+
+
 
 export default ListContacts
 
