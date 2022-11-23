966e8443631ea9c76d222cf73305cfdab109453b
Lynnmwova <82355571+Lynnmwova@users.noreply.github.com>
2022-11-23 18:43:02 +0300

    backend server and proptypes commits

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -1,4 +1,5 @@
-import React,{Component} from 'react'
+import React from 'react'
+import PropTypes from 'prop-types'
 
 function ListContacts (props){ 
   return(
@@ -27,6 +28,10 @@ className='contact-remove'> Remove </button>
   )
 }
 
+ListContacts.propTypes = {
+  contacts: PropTypes.array.isRequired,
+  onDeleteContact:PropTypes.func.isRequired
+}
 
 
 export default ListContacts
